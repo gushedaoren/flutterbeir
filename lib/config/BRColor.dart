7c9258d15b0d939c85432debb7250007a33115f5
oliver ni <gushedaoren@aliyun.com>
2020-06-20 21:49:27 +0800

    primary color

---
+++ lib/config/BRColor.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BrColors {
+
+
+  static const MaterialColor BRColorPrimary = const MaterialColor(
+    0xffF48433,
+    const <int, Color>{
+      50: const Color(0xffF48433),
+      100: const Color(0xffF48433),
+      200: const Color(0xffF48433),
+      300: const Color(0xffF48433),
+      400: const Color(0xffF48433),
+      500: const Color(0xffF48433),
+      600: const Color(0xffF48433),
+      700: const Color(0xffF48433),
+      800: const Color(0xffF48433),
+      900: const Color(0xffF48433),
+    },
+  );
+
+  static const Color colorPrimary = Color(0xffF48433);
+  static const Color colorPrimaryDark = Color(0xff388E3C);
+  static const Color colorAccent = Color(0xff8BC34A);
+  static const Color colorPrimaryLight = Color(0xffC8E6C9);
+
+  static const Color primaryText = Color(0xff212121);
+  static const Color secondaryText = Color(0xff757575);
+
+  static const Color dividerColor = Color(0xffBDBDBD);
+}
+
