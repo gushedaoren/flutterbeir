a7baae7311958b698275c673296381c7743d75fe
oliver ni <gushedaoren@aliyun.com>
2020-11-26 21:46:09 +0800

    ios 增加友盟统计

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,7 +1,11 @@
 #import "GeneratedPluginRegistrant.h"
 
 
-
+#import <UMCommon/UMCommon.h>
+//导入UMCommonLog的OC的头文件
+#import <UMCommonLog/UMCommonLogManager.h>
+//导入UMAnalytics的OC的头文件
+#import <UMCommon/MobClick.h>
 
 
 
--- lib/config/BRConfig.dart
+++ lib/config/BRConfig.dart
@@ -2,9 +2,9 @@
 
 
 class BRConfig {
-  static String domian="http://106.54.95.22:8083/";
+//  static String domian="http://106.54.95.22:8083/";
 
-//  static String domian="http://192.168.31.149:8081";
+  static String domian="http://192.168.31.149:8081";
 
   static String audioPlayerId="com.brstory";
 
